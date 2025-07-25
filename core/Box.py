c9fb10e528d747a06cec9dce14ab90450cbcb0c7
Eric Peyrouse <eric.peyrouse@ign.fr>
2025-07-25 11:28:58 +0200

    Ménage et documentation du code

--- core/Box.py
+++ core/Box.py
@@ -8,16 +8,23 @@ Updated on 15 dec. 2020
 from qgis.core import QgsCoordinateReferenceSystem, QgsCoordinateTransform, QgsProject
 
 
-# Représente une boite englobante
 class Box(object):
+    """
+    Classe représentant une boite englobante définie sur deux points.
+    """
 
     def __init__(self, xMin=None, yMin=None, xMax=None, yMax=None) -> None:
         """
-        Constructeur à partir de deux points
-        :param xMin coord X min
-        :param yMin coord y min
-        :param xMax coord X max
-        :param yMax coord Y max
+        Constructeur à partir de deux points.
+
+        :param xMin: coord X min
+        :type xMin: float
+        :param yMin: coord Y min
+        :type yMin: float
+        :param xMax: coord X max
+        :type xMax: float
+        :param yMax: coord Y max
+        :type yMax: float
         """
         if xMin is None or yMin is None or xMax is None or yMax is None:
             raise Exception("Impossible de continuer l'action en cours, la boite englobante de la zone de travail "
@@ -29,18 +36,33 @@ class Box(object):
         self.__YMax = max(yMin, yMax)
 
     def getXMin(self) -> float:
+        """:return: le X minimum"""
         return self.__XMin
 
     def getXMax(self) -> float:
+        """:return: le X maximum"""
         return self.__XMax
 
     def getYMin(self) -> float:
+        """:return: le Y minimum"""
         return self.__YMin
 
     def getYMax(self) -> float:
+        """:return: le Y maximum"""
         return self.__YMax
 
     def boxToStringWithSrid(self, sridProject, sridLayer) -> str:
+        """
+        Transformation des coordonnées en texte avec changement de projection.
+
+        :param sridProject: système de projection source (le projet QGIS)
+        :type sridProject: str
+
+        :param sridLayer: système de projection cible (la couche serveur)
+        :type sridLayer: str
+
+        :return: les coordonnées de la boite sous la forme : 'XMin,YMin,XMax,YMax'
+        """
         crsProject = QgsCoordinateReferenceSystem(sridProject)
         crsLayer = QgsCoordinateReferenceSystem(sridLayer)
         transformer = QgsCoordinateTransform(crsProject, crsLayer, QgsProject.instance())
--- core/CommunitiesMember.py
+++ core/CommunitiesMember.py
@@ -4,40 +4,59 @@ from ..PluginHelper import PluginHelper
 
 
 class CommunitiesMember(object):
+    """
+    Classe implémentant l'appartenance d'un utilisateur à des groupes.
+    """
 
     def __init__(self, url, tokenType, tokenAccess, proxy) -> None:
+        """
+        Constructeur.
+
+        :param url: la première partie de l'url (https://espacecollaboratif.ign.fr/)
+        :type url: str
+
+        :param tokenType: jeton sur le type de compte
+        :type tokenType: str
+
+        :param tokenAccess: jeton individuel d'accès
+        :type tokenAccess: str
+
+        :param proxy: le nom du serveur proxy
+        :type proxy: str
+
+        """
         self.__url = url
         self.__proxy = proxy
         self.__id = -1
         self.__username = ''
         # Liste des communautés de l'utilisateur
         self.__communities = []
-        # Liste des thèmes partagés
-        self.__sharedThemes = []
-        # Liste des thèmes actifs
-        self.__activeThemes = []
         self.__tokenType = tokenType
         self.__tokenAccess = tokenAccess
         self.__query = Query(url, proxy)
         # liste des noms des communautés de l'utilisateur
         self.__listNameOfCommunities = []
-        # Le nom de la communauté choisie par l'utilisateur lors de sa connexion
-        self.__nameActiveCommunity = ''
 
     def getUserCommunity(self, communityToFind) -> None:
+        """
+        Indique si un groupe appartient à l'utilisateur
+
+        :param communityToFind: le nom du groupe
+        :type communityToFind: str
+
+        :return: le nom du groupe ou None si l'utilisateur n'appartient pas au groupe cherché
+        """
         for community in self.__communities:
             if community.getName() == communityToFind:
                 return community
         return None
 
-    def setActiveCommunity(self, communityChoice) -> None:
-        self.__nameActiveCommunity = communityChoice
-
-    def getNameActiveCommunity(self) -> str:
-        return self.__nameActiveCommunity
-
-    # Récupère les informations sur un utilisateur
     def extractCommunities(self) -> []:
+        """
+        Lance une requête HTTP GET et récupère les informations sur un utilisateur.
+
+        :return: la liste des groupes auxquels appartient l'utilisateur
+        """
         self.__query.setHeaders(self.__tokenType, self.__tokenAccess)
         self.__query.setPartOfUrl("gcms/api/users/me")
         response = self.__query.simple()
@@ -57,6 +76,12 @@ class CommunitiesMember(object):
         return self.__listNameOfCommunities
 
     def getDatasCommunities(self, datas) -> None:
+        """
+        Récupère les caractéristiques de tous les groupes appartenant à l'utilisateur.
+
+        :param datas: les données issues d'une requête HTTP GET
+        :type datas: dict
+        """
         if len(datas) == 0:
             return
         params = {'url': self.__url, 'tokentype': self.__tokenType, 'tokenaccess': self.__tokenAccess,
@@ -67,20 +92,26 @@ class CommunitiesMember(object):
             self.__communities.append(community)
             self.__listNameOfCommunities.append({'name': community.getName(), 'id': community.getId()})
 
-    def getId(self):
+    def getId(self) -> int:
+        """
+        :return: l'identifiant utilisateur
+        """
         return self.__id
 
-    def getUserName(self):
+    def getUserName(self) -> str:
+        """
+        :return: le nom de l'utilisateur
+        """
         return self.__username
 
-    def getCommunities(self):
+    def getCommunities(self) -> []:
+        """
+        :return: la liste des groupes de l'utilisateur avec leurs caractéristiques
+        """
         return self.__communities
 
-    def getSharedThemes(self):
-        return self.__sharedThemes
-
-    def getActiveThemes(self):
-        return self.__activeThemes
-
-    def getListNameOfCommunities(self):
+    def getListNameOfCommunities(self) -> []:
+        """
+        :return: la liste des noms de groupes de l'utilisateur
+        """
         return self.__listNameOfCommunities
