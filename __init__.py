d30eb29bf31cff27d19200f5655f85bc4192844c
ADB-IGN <axeldebock@ign.fr>
2025-11-28 10:57:21 +0100

    Recompilation des fichiers Ui et Qrc sous PyQt6

--- __init__.py
+++ __init__.py
@@ -32,5 +32,5 @@ def classFactory(iface):  # pylint: disable=invalid-name
     type iface: QgsInterface
     """
     #
-    from .RipartModule import RipartPlugin
+    from .PluginModule import RipartPlugin
     return RipartPlugin(iface)
